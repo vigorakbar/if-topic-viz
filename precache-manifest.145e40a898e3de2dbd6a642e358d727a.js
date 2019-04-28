338aae954726162a7a5b8e12efbb7ec33848dd62
vigor <vigorakbar@gmail.com>
2019-04-29 00:20:32 +0700

    Updates

---
+++ precache-manifest.145e40a898e3de2dbd6a642e358d727a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e1e7999b58239eb2d5b",
+    "url": "/viz-if/static/js/runtime~main.11ae5be2.js"
+  },
+  {
+    "revision": "537a9573efac9c8e6356",
+    "url": "/viz-if/static/js/main.87ac58f9.chunk.js"
+  },
+  {
+    "revision": "42bbdd7998e25d59359f",
+    "url": "/viz-if/static/js/2.03d21f2d.chunk.js"
+  },
+  {
+    "revision": "537a9573efac9c8e6356",
+    "url": "/viz-if/static/css/main.336ad761.chunk.css"
+  },
+  {
+    "revision": "40e5922784b22027faceba8c40bf78c5",
+    "url": "/viz-if/index.html"
+  }
+];
